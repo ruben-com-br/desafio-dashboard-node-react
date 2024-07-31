902d012ee2bf2201670b5b483e1b5011cab3f266
Ruben Borges Ramos <ruben.ramos.ps@ojc.com.br>
2024-07-31 19:26:55 -0300

    remove props from build in netfly

--- client/src/components/BoxHeader.tsx
+++ client/src/components/BoxHeader.tsx
@@ -1,10 +1,32 @@
-import { Box } from "@mui/material";
-import { styled } from "@mui/system";
+import { Box, Typography, useTheme } from "@mui/material";
+import React from "react";
+import FlexBetween from "./FlexBetween";
 
-const FlexBetween = styled(Box)({
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-});
+type Props = {
+  title: string;
+  sideText: string;
+  subtitle?: string;
+  icon?: React.ReactNode;
+};
 
-export default FlexBetween;
+const BoxHeader = ({ icon, title, subtitle, sideText }: Props) => {
+  const { palette } = useTheme();
+  return (
+    <FlexBetween color={palette.grey[400]} margin="1.5rem 1rem 0 1rem">
+      <FlexBetween>
+        {icon}
+        <Box width="100%">
+          <Typography variant="h4" mb="-0.1rem">
+            {title}
+          </Typography>
+          <Typography variant="h6">{subtitle}</Typography>
+        </Box>
+      </FlexBetween>
+      <Typography variant="h5" fontWeight="700" color={palette.secondary[500]}>
+        {sideText}
+      </Typography>
+    </FlexBetween>
+  );
+};
+
+export default BoxHeader;

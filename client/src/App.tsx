f3bae705b7934b6a461ca7356a718df45e3b5ad2
Ruben Borges Ramos <ruben.ramos.ps@ojc.com.br>
2024-07-31 20:27:53 -0300

    add dashboard layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import { useMemo } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "./theme";
 import Navbar from "@/scenes/navbar";
-//import Dashboard from "@/scenes/dashboard";
+import Dashboard from "@/scenes/dashboard";
 //import Predictions from "@/scenes/predictions";
 
 function App() {
@@ -18,9 +18,9 @@ function App() {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
-              {/* <Route path="/" element={<Dashboard />} />
-              <Route path="/predictions" element={<Predictions />} /> */}
-              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/predictions" element={<Predictions />} /> */}
+              
               <Route path="/predictions" element={<div>prectitions page</div>} />
             </Routes>
           </Box>
---
+++ client/src/scenes/dashboard/Row3.tsx
@@ -0,0 +1,13 @@
+import DashboardBox from "@/components/DashboardBox";
+
+const Row3 = () => {
+  return (
+    <>
+      <DashboardBox bgcolor="#fff" gridArea="g"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="h"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="i"></DashboardBox>
+    </>
+  );
+};
+
+export default Row3;

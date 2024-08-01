8f7fefa5f73ba7d779e74ccc28320203506e16b5
Ruben Borges Ramos <ruben.ramos.ps@ojc.com.br>
2024-07-31 22:05:43 -0300

    server: Data modeling AND Key Performance Indicators Route

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,6 +1,10 @@
 import DashboardBox from "@/components/DashboardBox";
+import { useGetKpisQuery } from "@/state/api";
 
 const Row1 = () => {
+  const { data } = useGetKpisQuery();
+  console.log("data: ", data)
+
   return (
     <>
       <DashboardBox bgcolor="#fff" gridArea="a"></DashboardBox>
--- server/index.js
+++ server/index.js
@@ -5,13 +5,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-// import kpiRoutes from "./routes/kpi.js";
+import kpiRoutes from "./routes/kpi.js";
 // import productRoutes from "./routes/product.js";
 // import transactionRoutes from "./routes/transaction.js";
-// import KPI from "./models/KPI.js";
+import KPI from "./models/KPI.js";
 // import Product from "./models/Product.js";
 // import Transaction from "./models/Transaction.js";
-// import { kpis, products, transactions } from "./data/data.js";
+import { kpis, products, transactions } from "./data/data.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -26,7 +26,7 @@ app.use(cors());
 
 console.log('hello')
 /* ROUTES */
-// app.use("/kpi", kpiRoutes);
+app.use("/kpi", kpiRoutes);
 // app.use("/product", productRoutes);
 // app.use("/transaction", transactionRoutes);
 
@@ -40,9 +40,10 @@ mongoose
   .then(async () => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-//     /* ADD DATA ONE TIME ONLY OR AS NEEDED */
-//     // await mongoose.connection.db.dropDatabase();
-//     // KPI.insertMany(kpis);
+    /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+    /* Adiciona dados do data.js para o MongoDB */
+    await mongoose.connection.db.dropDatabase();
+    KPI.insertMany(kpis);
 //     // Product.insertMany(products);
 //     // Transaction.insertMany(transactions);
   })
